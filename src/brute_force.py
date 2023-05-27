9cfd91a2d5fb3771a12b48ae41edeb9d4adaf529
Mikolaj Piorczynski <m.piorczynski13@gmail.com>
2023-05-27 16:52:18 +0200

    Rearange for concistency with previous code

--- src/brute_force.py
+++ src/brute_force.py
@@ -1,40 +1,67 @@
-import itertools
+"""Finding a subtree with k vertices in a graph using a brute force."""
 import networkx as nx
+import math
+import random
+import numpy as np
+import itertools
+from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
+import argparse
+from tqdm import tqdm
+from texttable import Texttable
 
 
-def find_isomorphic_subgraph(graph, input_graph):
+def find_isomorphic_subtree(graph, tree):
     """
-    Function solves isomorphic subgraph problem (brute force approach).
-    :param graph: graph in which we are looking for an isomorphic subgraph
-    :param input_graph: graph isomorphic to the subgraph we are looking for
-    :return: isomorphic subgraph or None
+    Function solves tree subgraph isomorphism problem (brute force approach).
+    :param graph: graph in which we are looking for an isomorphic subtree
+    :param tree: tree isomorphic to the subtree we are looking for
+    :return: True or False
     """
-    nodes = list(graph.nodes())
-    input_nodes = list(input_graph.nodes())
-    input_size = len(input_nodes)
+    nodes = list(graph.nodes)
+    k = len(tree.nodes)
 
     # Generate all possible combinations of nodes for subgraph of input size
-    combinations = itertools.combinations(nodes, input_size)
-    for combination in combinations:
+    for combination in tqdm(itertools.combinations(nodes, k), desc="Looking for isomorphic subtree"):
         subgraph = graph.subgraph(combination)
-        if nx.is_isomorphic(subgraph, input_graph):
-            return subgraph
-    return None
+        if nx.is_isomorphic(subgraph, tree):
+            return True
+    return False
 
 
-if __name__ == '__main__':
-    # Example usage
-    G = nx.Graph()
-    G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 1)])
-    G.add_edges_from([(5, 6), (6, 7), (7, 8), (8, 5)])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run brute force.")
+    parser.add_argument("-n", type=int, default=100, help="Number of vertices in the graph.")
+    parser.add_argument("-k", type=int, default=5, help="Number of vertices in the tree.")
+    parser.add_argument("--seed", type=int, default=123, help="Random seed.")
 
-    input_G = nx.Graph()
-    input_G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 1)])
+    args = parser.parse_args()
 
-    isomorphic_subgraph = find_isomorphic_subgraph(G, input_G)
-    if isomorphic_subgraph:
-        print("Isomorphic subgraph found:")
-        print("Nodes: " + str(isomorphic_subgraph.nodes()))
-        print("Edges: " + str(isomorphic_subgraph.edges()))
-    else:
-        print("No isomorphic subgraph found.")
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+
+    t = Texttable()
+    t.add_rows([["Parameter", "Value"],
+                ["Number of vertices in the graph.", args.n],
+                ["Number of vertices in the tree.", args.k],
+                ["Random seed.", args.seed],
+    ])
+    print(t.draw())
+
+    # generate random graph using erdos renyi model
+    graph = nx.erdos_renyi_graph(args.n, p=0.1, seed=args.seed)
+    
+    # generate random tree
+    tree = nx.random_tree(args.k, seed=args.seed)
+
+    # plot graph and tree
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
+    ax1.set_title("Graph")
+    ax2.set_title("Tree")
+    nx.draw(graph, with_labels=False, width=0.1, node_size=10, ax=ax1)
+    nx.draw(tree, with_labels=False, width=0.1, node_size=10, ax=ax2)
+    fig.savefig("graph_tree.png")
+
+    # run brute force
+    flag = find_isomorphic_subtree(graph, tree)
+    print(f"Is there a isomorphic subtree with {args.k} vertices in the graph?: {flag}")
